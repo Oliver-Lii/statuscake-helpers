d2765ba327e7a9086e37bc44c9cc92c1a693b9a0
Oliver Li <oliver.1i@outlook.com>
2023-05-08 14:23:52 +0100

    Update documentation on parameters & add examples

--- StatusCake-Helpers/Public/Uptime/Metadata/Get-StatusCakeHelperUptimeAlert.ps1
+++ StatusCake-Helpers/Public/Uptime/Metadata/Get-StatusCakeHelperUptimeAlert.ps1
@@ -3,29 +3,35 @@
 .SYNOPSIS
     Returns a list of uptime check alerts for a test
 .DESCRIPTION
-    Returns all alerts that have been sent for a test by its name or id. The return order is newest alerts are shown first.
-    Alerts to be returned can be filtered by date using the Before parameter. The number of alerts returned can be controlled
+    Returns all alerts that have been triggered for a test by its name or id. The return order is newest alerts are shown first.
+    Alerts to be returned can be filtered by date using the After/Before parameters. The number of alerts returned can be controlled
     using the Limit argument.
 .PARAMETER APICredential
     Credentials to access StatusCake API
 .PARAMETER ID
-    ID of the Test to retrieve the sent alerts for
+    ID of the Test to retrieve the triggered alerts for
 .PARAMETER Name
-    Name of the Test to retrieve the sent alerts for
+    Name of the Test to retrieve the triggered alerts for
 .PARAMETER Before
-    Return only results from before this date
+    Return only alerts triggered before this date
 .PARAMETER After
-    Return only results after this date
+    Return only alerts triggered after this date
 .PARAMETER Limit
     The maximum of number of results to return
 .OUTPUTS
-    Returns an object with the details on the Alerts Sent
+    Returns an object with the details on the alerts triggered
 .EXAMPLE
     C:\PS> Get-StatusCakeHelperUptimeAlert -ID 123456 -Before "2017-08-19 13:29:49"
-    Return all the alerts sent for test ID 123456 since the 19th August 2017 13:29:49
+    Return all the alerts triggered for test ID 123456 since the 19th August 2017 13:29:49
+.EXAMPLE
+    C:\PS> Get-StatusCakeHelperUptimeAlert -ID 123456 -After "2018-01-01 00:00:00" -Before "2019-01-01 00:00:00"
+    Return all the alerts triggered for test ID 123456 after the 1st January 2018 but before 1st January 2019
 .EXAMPLE
     C:\PS> Get-StatusCakeHelperUptimeAlert -ID 123456 -Limit 100
-    Return the last 100 alerts sent for test ID 123456
+    Return the last 100 alerts triggered for test ID 123456
+.EXAMPLE
+    C:\PS> Get-StatusCakeHelperUptimeAlert -ID 123456 -Limit 1
+    Return the most recent alert triggered for test ID 123456
 .LINK
     https://github.com/Oliver-Lii/statuscake-helpers/blob/master/Documentation/Uptime/Base/Get-StatusCakeHelperUptimeAlert.md
 .LINK
--- StatusCake-Helpers/Public/Uptime/Metadata/Get-StatusCakeHelperUptimeHistory.ps1
+++ StatusCake-Helpers/Public/Uptime/Metadata/Get-StatusCakeHelperUptimeHistory.ps1
@@ -3,28 +3,31 @@
 .SYNOPSIS
     Returns uptime check history results for tests
 .DESCRIPTION
-    Returns uptime check history results for tests detailing the runs performed on the StatusCake testing infrastructure. The return order is newest alerts are shown first.
-    Alerts to be returned can be filtered by date using the Before parameter.
+    Returns uptime check history results for tests detailing the runs performed on the StatusCake testing infrastructure. The most recent history results are shown first.
+    Alerts to be returned can be filtered by date using the After/Before parameters.
 .PARAMETER APICredential
     Credentials to access StatusCake API
 .PARAMETER ID
-    ID of the Test to retrieve the sent alerts for
+    ID of the Test to retrieve the history results for
 .PARAMETER Name
-    Name of the Test to retrieve the sent alerts for
+    Name of the Test to retrieve the history results for
 .PARAMETER Before
-    Return only results from before this date
+    Return only history results created before this date
 .PARAMETER After
-    Return only results after this date
+    Return only history results created after this date
 .PARAMETER Limit
     The maximum of number of results to return
 .OUTPUTS
-    Returns an object with the details on the Alerts Sent
+    Returns an object with the details from a StatusCake test run
 .EXAMPLE
     C:\PS> Get-StatusCakeHelperUptimeHistory -ID 123456 -Before "2017-08-19 13:29:49"
     Return all the uptime history for test ID 123456 since the 19th August 2017 13:29:49
 .EXAMPLE
     C:\PS> Get-StatusCakeHelperUptimeHistory -ID 123456 -Limit 100
-    Return the last 100 historical results sent for test ID 123456
+    Return the last 100 historical results for test ID 123456
+.EXAMPLE
+    C:\PS> Get-StatusCakeHelperUptimeHistory -ID 123456 -Limit 1
+    Return the most recent historical result for test ID 123456
 .LINK
     https://github.com/Oliver-Lii/statuscake-helpers/blob/master/Documentation/Uptime/Property/Get-StatusCakeHelperUptimeHistory.md
 .LINK
